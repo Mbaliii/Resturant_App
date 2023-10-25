9c52ef20ac4181aa188e7a2dc58d679304654882
Mbali <mbalikhumalo408@gmail.com>
2023-10-25 13:14:50 +0200

    resturant

--- screens/Booking.js
+++ screens/Booking.js
@@ -1,79 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
 
 const BookingScreen = ({ navigation }) => {
-    const [name, setName] = React.useState('');
-    const [partySize, setPartySize] = React.useState('');
-    const [date, setDate] = React.useState('');
-    const [time, setTime] = React.useState('');
+  const [name, setName] = useState('');
+  const [partySize, setPartySize] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
 
-    const handleReservation = () => {
-        if (!name || !partySize || !date || !time) {
-            Alert.alert('Please fill in all fields');
-        } else {
+  const handleReservation = () => {
+    if (!name || !partySize || !date || !time) {
+      Alert.alert('Please fill in all fields');
+    } else {
+      const newBooking = {
+        id: Date.now(), 
+        name,
+        partySize,
+        date,
+        time,
+      };
 
-            Alert.alert(
-                'Booking Successful',
-                'Thank you for booking!',
-                [
-                    {
-                        text: 'OK',
-                        onPress: () => {
-                            navigation.navigate('ReservationList'); 
-                        },
-                    },
-                ]
-            );
-        }
-    };
+      navigation.navigate('ReservationList', { newBooking });
+    }
+  };
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Restaurant Reservation</Text>
-            <TextInput
-                style={styles.input}
-                placeholder="Your Name"
-                onChangeText={text => setName(text)}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Party Size"
-                keyboardType="numeric"
-                onChangeText={text => setPartySize(text)}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Date (YYYY-MM-DD)"
-                onChangeText={text => setDate(text)}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Time (HH:MM AM/PM)"
-                onChangeText={text => setTime(text)}
-            />
-            <Button title="Book Now" onPress={handleReservation} />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Restaurant Reservation</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Your Name"
+        onChangeText={text => setName(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Party Size"
+        keyboardType="numeric"
+        onChangeText={text => setPartySize(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Date (YYYY-MM-DD)"
+        onChangeText={text => setDate(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Time (HH:MM AM/PM)"
+        onChangeText={text => setTime(text)}
+      />
+      <Button title="Book Now" onPress={handleReservation} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    title: {
-        fontSize: 24,
-        marginBottom: 20,
-    },
-    input: {
-        width: 300,
-        height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
-        marginBottom: 10,
-        padding: 10,
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff', 
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    fontWeight: 'bold', 
+  },
+  input: {
+    width: 300,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 10,
+  },
 });
 
 export default BookingScreen;
--- screens/ReservationList.js
+++ screens/ReservationList.js
@@ -1,14 +1,34 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
+const ReservationList = ({ route }) => {
+    const { newBooking } = route.params;
 
-const ReservationList = () => {
     return (
-        <View>
-            <Text>Reservation</Text>
+        <View style={styles.container}>
+            <Text style={styles.historyTitle}>Recent Bookings</Text>
+            <Text style={styles.bookingItem}>
+                {newBooking.name}, Party of {newBooking.partySize}, {newBooking.date}, {newBooking.time}
+            </Text>
         </View>
-    )
+    );
 };
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        padding: 20,
+    },
+    historyTitle: {
+        fontSize: 24,
+        marginBottom: 10,
+        fontWeight: 'bold',
+    },
+    bookingItem: {
+        fontSize: 18,
+        marginBottom: 5,
+    },
+});
 
-export default ReservationList
+export default ReservationList;
